e0dbf100f160d722a373007f5ec9d3ca2e8a864d
dmtrth25 <vodolazhchenko25@gmail.com>
2023-08-08 14:36:36 +0300

    config for deploy view

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '32': '32px'
+      },
+      boxShadow: {
+        'custom': '0 4px 8px rgba(0, 0, 0, 0.1)',
+      },
+      backgroundColor: {
+        'custom': '#6c6a6a',
+        'hover': '#e0d4d4'
+      },
+    },
   },
   plugins: [],
 }
